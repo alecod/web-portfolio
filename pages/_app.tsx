cad239c5ad8c47442071b9de3e2c2e4c7780e55e
Alessandro Spoto <alecodx@gmail.com>
2024-05-29 18:26:31 -0300

    matomo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,8 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import React from 'react';
 
-
-
-const MyApp = ({ Component, pageProps }: AppProps) => {
-  React.useEffect(() => {
-    var _mtm = window._mtm = window._mtm || [];
-    _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.async=true; g.src='https://cdn.matomo.cloud/webportfolioonerustvercelapp.matomo.cloud/container_BW6w2jxn.js'; s.parentNode.insertBefore(g,s);
-   }, [])
-
-  return (
-
+const MyApp = ({ Component, pageProps }: AppProps) => (
   <Component {...pageProps} />
-
 )
-}
 
 export default MyApp
