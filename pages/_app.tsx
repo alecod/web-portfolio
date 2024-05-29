7f050a59069fd1f03a83cf205853f6f0487d0bb2
Alessandro Spoto <alecodx@gmail.com>
2024-05-29 18:23:30 -0300

    matomo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import React from 'react';
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
+
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  React.useEffect(() => {
+    var _mtm = window._mtm = window._mtm || [];
+    _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true; g.src='https://cdn.matomo.cloud/webportfolioonerustvercelapp.matomo.cloud/container_BW6w2jxn.js'; s.parentNode.insertBefore(g,s);
+   }, [])
+
+  return (
+
   <Component {...pageProps} />
+
 )
+}
 
 export default MyApp
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
@@ -12,19 +13,7 @@ export default function Document() {
         <Main />
         <NextScript />
 
-        <script>
-  var _paq = window._paq = window._paq || [];
-  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-  _paq.push(['trackPageView']);
-  _paq.push(['enableLinkTracking']);
-  (function() {
-    var u="https://webportfolioonerustvercelapp.matomo.cloud/";
-    _paq.push(['setTrackerUrl', u+'matomo.php']);
-    _paq.push(['setSiteId', '1']);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.async=true; g.src='https://cdn.matomo.cloud/webportfolioonerustvercelapp.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
-  })();
-</script>
+    
 
       </body>
 
