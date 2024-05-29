3f77ff622a4cd55203837e01bd0fac4cd5f22999
Alessandro Spoto <alecodx@gmail.com>
2024-05-29 18:12:50 -0300

    matomo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,7 +11,24 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+
+        <script>
+  var _paq = window._paq = window._paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="https://webportfolioonerustvercelapp.matomo.cloud/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '1']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true; g.src='https://cdn.matomo.cloud/webportfolioonerustvercelapp.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+
       </body>
+
+   
     </Html>
   )
 }
