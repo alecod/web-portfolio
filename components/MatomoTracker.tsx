792f6bf26b8437f3cb70d2c3e83ac5b8ae95dbe1
Alessandro Junqueira <alecodx@gmail.com>
2024-06-03 16:18:13 -0300

    up

--- components/MatomoTracker.tsx
+++ components/MatomoTracker.tsx
@@ -13,7 +13,7 @@ const MatomoTracker = () => {
     window._paq.push(['trackPageView']);
     window._paq.push(['enableLinkTracking']);
     (() => {
-      const u = "http://localhost:8080/";
+      const u = "http://localhost:8888/";
       window._paq.push(['setTrackerUrl', u + 'matomo.php']);
       window._paq.push(['setSiteId', '1']);
       const d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
